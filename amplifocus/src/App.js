c511e390e4f55dc1ad07ef0f32728a8dabd3a9aa
yuyiwang <yuyiwangxia@gmail.com>
2021-11-15 15:18:45 -0300

    Created amplifoucs main code dependencies

---
+++ amplifocus/src/App.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import "./App.css";
+
+function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Comida pro Peixe",
+      day: "Mar 12th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Lavarzito lo banheirito",
+      day: "Dec 8th at 2:30pm",
+      reminder: true,
+    },
+  ]);
+
+  //Add Task
+  const addTask = (task) => {
+    console.log(task);
+    const id = tasks.length + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
+  //Delete Task
+  const deleteTask = (id) => {
+    //console.log("delete" + id);
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
+  return (
+    <div className="container">
+      <Header
+        title="Task Tracker"
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks To Show"
+      )}
+    </div>
+  );
+}
+
+export default App;
